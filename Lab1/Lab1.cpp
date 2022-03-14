feb14599069519b40ab901d5effc6d42a47219ed
christoforoschari <christoforos_chari@hotmail.com>
2022-03-14 19:44:47 +0200

    random shapes

--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -1,17 +1,20 @@
 //Programmer:Christoforos Chari
 #include<iostream>
+#include<cstdlib>
+#include<ctime>
 using namespace std;
 
 void menu() {
-	cout << "1) Draw a horizontal line";
+	cout << "1)Draw a horizontal line";
 	cout << "\n2)Draw a vertical line";
 	cout << "\n3)Draw a square";
 	cout << "\n4)Draw a rectangle";
-	cout << "\n5)Exit";
+	cout << "\n5)Draw a random shapes";
+	cout << "\n6)Exit";
 	cout << "\nEnter your choise ";
 }
 void drawHorizontalLine(const int length, const char ch) {
-
+	cout << "Horizontal Line( " << length << ", " << ch << ")\n";
 	for (int i = 0; i < length; i++) {
 		cout << ch;
 
@@ -19,12 +22,14 @@ void drawHorizontalLine(const int length, const char ch) {
 }
 
 void drawVerticalLine(const int height, const char ch) {
+	cout << "Vertical Line( " << height << ", " << ch << ")\n";
 	for (int i = 0; i < height; i++)
 		cout << ch << endl;
 }
 
 void drawSquare(const int size, const char ch) {
 	int height = size; int length = size;
+	cout << "Square( " << size << ", " << ch << ")\n";
 	for (int i = 1; i <= height; i++)
 	{
 		for (int j = 1; j <= length; j++)
@@ -42,12 +47,9 @@ void drawSquare(const int size, const char ch) {
 	}
 
 }
-
-
-
 
 void drawRectangle(const int height, const int length, const char ch) {
-
+	cout << "Rectangle( "<<height<<", "<< length << ", " << ch << ")\n";
 	for (int i = 1; i <= height; i++)
 	{
 		for (int j = 1; j <= length; j++)
@@ -66,13 +68,57 @@ void drawRectangle(const int height, const int length, const char ch) {
 
 }
 
-int main() {
-	int choise, num, num1;
-	char ch;
+void drawRandomShapes(int numShapes) {
+	for (int i = 0; i < numShapes; i++) {
+		cout << "\nDraws randoms shapes\n";
+		int ShapeType = 1 + rand() % 4;
+		int height = 1 + rand() % 20;
+		int length = 1 + rand() % 20;
+		char sympol = rand() % 94;//33-126
 
+		
+
+			switch (ShapeType) {
+			case 1:
+				
+				drawHorizontalLine(length, sympol);
+				break;
+			case 2:
+				
+				drawRectangle(height, length, sympol);
+				break;
+			case 3:
+				
+				drawSquare(length, sympol);
+				break;
+			case 4:
+				
+				drawVerticalLine(length, sympol);
+				break;
+			default :
+				cout << "Wrong input";
+		
+			}
+
+		
+	}
+
+}
+
+
+
+
+
+
+int main() {
+	const int MAX_ARRAY = 10;
+	int choise, num, num1, tmp;
+	char ch;
+	srand((unsigned int)time(NULL));
 	do {
 		menu();
 		cin >> choise;
+		
 		switch (choise) {
 		case 1:
 			cout << "\nEnter the length number:";
@@ -113,18 +159,19 @@ int main() {
 			drawRectangle(num, num1, ch);
 
 			break;
-		default:
-			if (choise > 5) {
-				cout << "\nThe choise is 1-5 try agin.\n";
-			}
+		case 5:
 
+			drawRandomShapes(MAX_ARRAY);
+
+			break;
 		}
 
-	} while (choise != 5); {
+	} while (choise != 6); {
 		cout << "\nThankyou have a nice day";
 
 	}
-
-
-	return 0;
+	
+	
+	
+		return 0;
 }
